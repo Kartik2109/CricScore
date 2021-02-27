cd20a2e55af74c4cfb762b5fd445d68c34ecef82
Kartik Sharma <ks2109kartik@gmail.com>
2021-02-28 01:39:08 +0530

    livescore

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from "./components/NavBar";
+import TabComponent from "./components/TabComponent";
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <TabComponent/>
+    </>
+  );
+}
+
+export default App;
